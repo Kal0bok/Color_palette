71bc0c45688b5e58bd8cf2026dabf027bad4b039
Bogdans <bogdans.cumaks@sk.lvt.lv>
2026-02-14 19:16:25 +0300

    initial UI layout for ColorPicker

--- src/simple_pipette.java
+++ src/simple_pipette.java
@@ -1,9 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class simple_pipette {
+public class ColorPickerApp extends JFrame {
+    protected JLabel colorPreview;
+    protected JTextArea colorInfo;
+    protected JButton pickButton;
 
-	public static void main(String[] args) {
-		//start
+    public ColorPickerApp() {
+        setTitle("Java Color Picker");
+        setSize(400, 350);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(10, 10));
 
-	}
+        colorPreview = new JLabel(" ", SwingConstants.CENTER);
+        colorPreview.setOpaque(true);
+        colorPreview.setPreferredSize(new Dimension(100, 100));
+        colorPreview.setBorder(BorderFactory.createLineBorder(Color.GRAY));
 
+        colorInfo = new JTextArea("Нажмите кнопку, чтобы начать...");
+        colorInfo.setEditable(false);
+
+        pickButton = new JButton("Взять пипетку");
+
+        add(pickButton, BorderLayout.NORTH);
+        add(colorPreview, BorderLayout.CENTER);
+        add(new JScrollPane(colorInfo), BorderLayout.SOUTH);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ColorPickerApp().setVisible(true));
+    }
 }
